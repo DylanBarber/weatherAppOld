9f9d83bcd0e2768cc2a653651622f6f606b2c8ca
Dylan Barber <DylanBarber1@Outlook.com>
2019-08-23 10:16:06 -0400

    Add env

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import dotenv from 'dotenv'
+//Render entry box for City, State 
+// Do API call for lat long
+// Do API call for weather
+// Return Weather
 
-function App() {
+dotenv.config()
+
+
+
+class App extends React.Component {
+  inputOnChangeHandler = (e) => {
+    this.setState({inputText: e.target.value})
+  }
+  locationHandler = async () => {
+    let locationFetch = await fetch(`http://open.mapquestapi.com/geocoding/v1/address?key=${process.env.MAPQUEST_KEY}&city=${this.state.inputText}`)
+    let locationData = await locationFetch.json()
+    let latitude = locationData.results[0].locations[0].latLng.lat
+    let longitude = locationData.results[0].locations[0].latLng.lng
+    let weatherFetch = await fetch(`https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${latitude},${longitude}`)
+    let weatherData = await weatherFetch.json()
+    console.log(weatherData)
+  }
+
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input onChange={this.inputOnChangeHandler}/>
+      <button onClick={this.locationHandler}>Request</button>
     </div>
   );
+  }
 }
 
 export default App;
